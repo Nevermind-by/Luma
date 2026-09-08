c7b74f2e9b3e5bcc8b3db8a3d36039001c76cc34
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-09-08 16:00:51 +0300

    Add distribution variant to AppsTorrent release

--- Luma/Core/Models/AppsTorrentRelease.swift
+++ Luma/Core/Models/AppsTorrentRelease.swift
@@ -4,5 +4,6 @@ nonisolated struct AppsTorrentRelease: Hashable, Sendable {
     let title: String
     let version: SoftwareVersion
     let pageURL: URL
+    let distributionVariant: AppsTorrentDistributionVariant
     let downloadOptions: [DownloadOption]
 }
