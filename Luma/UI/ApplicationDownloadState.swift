81fc5b1ede0083414173f13fe9e192d561a0a849
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-09-08 19:21:45 +0300

    Extend download state through installation

--- Luma/UI/ApplicationDownloadState.swift
+++ Luma/UI/ApplicationDownloadState.swift
@@ -4,5 +4,7 @@ nonisolated enum ApplicationDownloadState: Equatable, Sendable {
     case notStarted
     case downloading(DownloadProgress)
     case readyToInstall(PreparedUpdate)
+    case installing(SoftwareVersion)
+    case installed(SoftwareVersion)
     case failed(String)
 }
