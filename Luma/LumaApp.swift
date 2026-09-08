d6e696cac1b90edc4654f9078dce3eab87a4f298
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-09-08 14:14:01 +0300

    chore: initialize Luma macOS app

---
+++ Luma/LumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumaApp.swift
+//  Luma
+//
+//  Created by Nevermind on 08.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct LumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
