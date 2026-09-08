d22f14bb6881d3f9050023085fdfe28444193814
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-09-08 19:19:59 +0300

    Persist authorized install destination

---
+++ Luma/Core/Services/InstallDestinationStore.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+nonisolated struct InstallDestinationStore: Sendable {
+    private let defaults: UserDefaults
+    private let bookmarkKey = "Luma.installDestinationBookmark"
+
+    init(defaults: UserDefaults = .standard) {
+        self.defaults = defaults
+    }
+
+    func savedDirectory() -> URL? {
+        guard let data = defaults.data(forKey: bookmarkKey) else { return nil }
+
+        var isStale = false
+        guard let url = try? URL(
+            resolvingBookmarkData: data,
+            options: [.withSecurityScope, .withoutUI],
+            relativeTo: nil,
+            bookmarkDataIsStale: &isStale
+        ) else {
+            defaults.removeObject(forKey: bookmarkKey)
+            return nil
+        }
+
+        if isStale {
+            save(directory: url)
+        }
+
+        return url
+    }
+
+    func save(directory: URL) {
+        guard let data = try? directory.bookmarkData(
+            options: [.withSecurityScope],
+            includingResourceValuesForKeys: nil,
+            relativeTo: nil
+        ) else {
+            return
+        }
+
+        defaults.set(data, forKey: bookmarkKey)
+    }
+
+    func clear() {
+        defaults.removeObject(forKey: bookmarkKey)
+    }
+}
