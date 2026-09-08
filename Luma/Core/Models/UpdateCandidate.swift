eb5d97b4cd4ab123986d4181a1ae828f0996bb17
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-09-08 16:00:57 +0300

    Add distribution variant to update candidate

--- Luma/Core/Models/UpdateCandidate.swift
+++ Luma/Core/Models/UpdateCandidate.swift
@@ -4,17 +4,20 @@ nonisolated struct UpdateCandidate: Identifiable, Hashable, Sendable {
     let id: UUID
     let application: ApplicationIdentity
     let version: SoftwareVersion
+    let distributionVariant: AppsTorrentDistributionVariant?
     let downloadOptions: [DownloadOption]
 
     init(
         id: UUID = UUID(),
         application: ApplicationIdentity,
         version: SoftwareVersion,
+        distributionVariant: AppsTorrentDistributionVariant? = nil,
         downloadOptions: [DownloadOption]
     ) {
         self.id = id
         self.application = application
         self.version = version
+        self.distributionVariant = distributionVariant
         self.downloadOptions = downloadOptions
     }
 }
