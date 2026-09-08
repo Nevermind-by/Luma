32471af367de0367bb2f8616f2b207446a96e445
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-09-08 16:09:45 +0300

    Preserve backwards-compatible application initialization

--- Luma/Core/Models/InstalledApplication.swift
+++ Luma/Core/Models/InstalledApplication.swift
@@ -6,4 +6,18 @@ nonisolated struct InstalledApplication: Identifiable, Hashable, Sendable {
     let version: SoftwareVersion
     let bundleURL: URL
     let installationSource: ApplicationInstallationSource
+
+    init(
+        id: ApplicationIdentity,
+        name: String,
+        version: SoftwareVersion,
+        bundleURL: URL,
+        installationSource: ApplicationInstallationSource = .unknown
+    ) {
+        self.id = id
+        self.name = name
+        self.version = version
+        self.bundleURL = bundleURL
+        self.installationSource = installationSource
+    }
 }
