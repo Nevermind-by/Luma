e0d093986c09678a9f6b34ff2f75f0c8995f1a7e
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-09-09 13:24:21 +0300

    Model prepared installer payload

--- Luma/Core/Models/PreparedUpdate.swift
+++ Luma/Core/Models/PreparedUpdate.swift
@@ -5,25 +5,36 @@ nonisolated struct PreparedUpdate: Identifiable, Equatable, Sendable {
     let application: ApplicationIdentity
     let version: SoftwareVersion
     let artifactURL: URL
-    let applicationURL: URL
-    let bundleIdentifier: String
-    let stagingDirectoryURL: URL
+    let payload: PreparedUpdatePayload
+    let bundleIdentifier: String?
+    let stagingDirectoryURL: URL?
 
     init(
         id: UUID = UUID(),
         application: ApplicationIdentity,
         version: SoftwareVersion,
         artifactURL: URL,
-        applicationURL: URL,
-        bundleIdentifier: String,
-        stagingDirectoryURL: URL
+        payload: PreparedUpdatePayload,
+        bundleIdentifier: String? = nil,
+        stagingDirectoryURL: URL? = nil
     ) {
         self.id = id
         self.application = application
         self.version = version
         self.artifactURL = artifactURL
-        self.applicationURL = applicationURL
+        self.payload = payload
         self.bundleIdentifier = bundleIdentifier
         self.stagingDirectoryURL = stagingDirectoryURL
+    }
+
+    var installerURL: URL {
+        payload.url
+    }
+
+    var isApplicationBundle: Bool {
+        if case .application = payload {
+            return true
+        }
+        return false
     }
 }
