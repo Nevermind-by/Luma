7a7792ae6b703f116c1708d738ea661a7e3e87c7
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-09-09 16:19:41 +0300

    Expand AppsTorrent parser diagnostics tests

--- LumaTests/AppsTorrentPageParserTests.swift
+++ LumaTests/AppsTorrentPageParserTests.swift
@@ -30,11 +30,36 @@ struct AppsTorrentPageParserTests {
         #expect(release.title == "Parallels Desktop 27")
         #expect(release.version == SoftwareVersion("27.0.1-58670"))
         #expect(release.pageURL == pageURL)
+        #expect(release.distributionVariant == .standard)
         #expect(release.downloadOptions.count == 3)
         #expect(release.downloadOptions.map(\.title) == ["Direct link", "Mail.ru", "MediaFire"])
         #expect(release.downloadOptions[0].url.absoluteString == "https://example.com/direct.iso")
         #expect(release.downloadOptions[1].url.absoluteString == "https://cloud.mail.ru/public/test/path")
         #expect(release.downloadOptions[2].url.absoluteString == "https://www.mediafire.com/file/test/file")
+    }
+
+    @Test func detectsMASDistributionVariant() throws {
+        let html = """
+        <h1 itemprop="name">Parallels Desktop 27 [MAS]</h1>
+        <span itemprop="softwareVersion">27.0.1-58670 [MAS]</span>
+        <!--dle_spoiler Parallels Desktop 27.0.1-58670 [MAS] -->
+        <a href="https://example.com/mas.pkg">Прямая ссылка</a>
+        """
+        let pageURL = URL(string: "https://appstorrent.ru/61-parallels-desktop.html")!
+
+        let release = try parser.parse(html: html, pageURL: pageURL)
+
+        #expect(release.distributionVariant == .mas)
+        #expect(release.version == SoftwareVersion("27.0.1-58670 [MAS]"))
+    }
+
+    @Test func rejectsPageWithoutTitle() {
+        let html = "<span itemprop=\"softwareVersion\">1.2.3</span>"
+        let pageURL = URL(string: "https://appstorrent.ru/some-app.html")!
+
+        #expect(throws: AppsTorrentPageParser.ParserError.missingTitle) {
+            try parser.parse(html: html, pageURL: pageURL)
+        }
     }
 
     @Test func rejectsPageWithoutVersion() {
@@ -54,6 +79,20 @@ struct AppsTorrentPageParserTests {
         let pageURL = URL(string: "https://appstorrent.ru/some-app.html")!
 
         #expect(throws: AppsTorrentPageParser.ParserError.missingCurrentReleaseBlock) {
+            try parser.parse(html: html, pageURL: pageURL)
+        }
+    }
+
+    @Test func rejectsPageWithoutSupportedDownloadLinks() {
+        let html = """
+        <h1 itemprop="name">Some App</h1>
+        <span itemprop="softwareVersion">1.2.3</span>
+        <!--dle_spoiler Some App 1.2.3 -->
+        <a href="https://example.com/unknown">Download mirror</a>
+        """
+        let pageURL = URL(string: "https://appstorrent.ru/some-app.html")!
+
+        #expect(throws: AppsTorrentPageParser.ParserError.invalidDownloadURL) {
             try parser.parse(html: html, pageURL: pageURL)
         }
     }
