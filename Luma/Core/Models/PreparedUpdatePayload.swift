ab7ca7bdccbcf5b465e910921e5205152fde205d
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-09-09 13:43:00 +0300

    Support generic external update installers

--- Luma/Core/Models/PreparedUpdatePayload.swift
+++ Luma/Core/Models/PreparedUpdatePayload.swift
@@ -4,10 +4,11 @@ nonisolated enum PreparedUpdatePayload: Equatable, Sendable {
     case application(URL)
     case diskImage(URL)
     case package(URL)
+    case externalInstaller(URL)
 
     var url: URL {
         switch self {
-        case .application(let url), .diskImage(let url), .package(let url):
+        case .application(let url), .diskImage(let url), .package(let url), .externalInstaller(let url):
             return url
         }
     }
