4c4e0a58c6218b2d6a258750e8c996c13e8e625d
Maksim Karbovsky <m.karbovsky92@gmail.com>
2026-09-20 18:10:41 +0300

    Усилить восстановление install bookmark

--- Luma/Core/Services/InstallDestinationStore.swift
+++ Luma/Core/Services/InstallDestinationStore.swift
@@ -23,7 +23,16 @@ struct InstallDestinationStore {
         }
 
         if isStale {
-            save(directory: url)
+            guard let refreshedBookmark = try? url.bookmarkData(
+                options: [.withSecurityScope],
+                includingResourceValuesForKeys: nil,
+                relativeTo: nil
+            ) else {
+                defaults.removeObject(forKey: bookmarkKey)
+                return nil
+            }
+
+            defaults.set(refreshedBookmark, forKey: bookmarkKey)
         }
 
         return url
